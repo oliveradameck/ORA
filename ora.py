8a49073d7ffd0c61db500479e071896d8176b06c
Oliver Adameck <oliver.adameck@gmail.com>
2019-07-01 14:04:51 +0200

    Changed ampl pathes

--- ora.py
+++ ora.py
@@ -2,6 +2,7 @@ from amplpy import AMPL, Environment, DataFrame
 import pandas as pd
 import numpy as np
 import sys
+import os
 
 from ampl_modules.amplcode import AmplCode
 
@@ -33,8 +34,11 @@ print("Optimized Room Assignment Tool")
 print("Setup")
 print("Expected Number of Courses: ", sum(list(course_frequency.values())))
 
-pro_environment = "/home/paszin/ampl-pro/ampl_linux-intel64"
-low_environment = "/home/paszin/ampl/ampl.linux64"
+# pro_environment = "/Users/oliveradameck/Desktop/ampl-pro/ampl_linux-intel64"
+# low_environment = "/User/oliveradameck/Desktopampl/ampl.linux64"
+pro_environment = "ampl"
+# low_environment = "/User/oliveradameck/Desktopampl/ampl.linux64"
+# path = os.path.join(os.path.expanduser('~'), 'ora')
 ampl = AMPL(Environment(pro_environment))
 ampl.setOption('solver', 'cplex')
 
